f705d620523bb02842e53f15c8ee4cc1525fe81d
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2025-12-24 15:28:03 -0700

    chore: update permission class to only allow a user to see their corresponding notes

--- backend/note/views.py
+++ backend/note/views.py
@@ -4,26 +4,27 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Note
 
 # Create your views here.
+
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        queryset = Note.objects.all()
-        course = self.request.query_params.get("course")
-        if course:
-            queryset = queryset.filter(course=course)
+        user = self.request.user
+        queryset = Note.objects.filter(author=user).select_related('course')
+        
+        course_id = self.request.query_params.get("course")
+        if course_id:
+            queryset = queryset.filter(course_id=course_id)
+            
         return queryset
 
     def perform_create(self, serializer):
-        serializer.is_valid(raise_exception=True)
         serializer.save(author=self.request.user)
 
 
 class NoteDelete(generics.DestroyAPIView):
-    serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user
-        return Note.objects.filter(author=user)
+        return Note.objects.filter(author=self.request.user)
