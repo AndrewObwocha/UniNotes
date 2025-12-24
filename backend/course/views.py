ad9e2f303eb0d474e2afff3f94fb384f0ee1af83
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2025-12-24 15:25:04 -0700

    feat: add filtering capabilities of listed courses

--- backend/course/views.py
+++ backend/course/views.py
@@ -1,11 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from rest_framework.permissions import IsAuthenticated
 from .models import Course
 from .serializers import CourseSerializer
-from rest_framework.permissions import AllowAny
 
-
-# Create your views here.
 class CourseList(generics.ListAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'code', 'instructor']
