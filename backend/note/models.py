babb2ed1e90a2a0083d993690eae11c2e61fcb1a
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2025-12-24 15:26:43 -0700

    feat: import author directly from settings and order notes by date creation

--- backend/note/models.py
+++ backend/note/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from course.models import Course
 
-# Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='courses')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notes")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='notes')
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
