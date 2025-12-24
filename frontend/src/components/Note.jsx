5fadc5aa84c556ea26fcc9daf29b902d79dcb23b
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2025-12-24 14:20:13 -0700

    style: change note styling

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,18 +1,20 @@
-import "../styles/Note.css"
+import "../styles/Note.css";
 
 function Note({ note, onDelete }) {
-    const formattedDate = new Date(note.created_at).toLocaleDateString('en-US')
-    
-    return (
-        <div className='note-container'>
-            <p className='note-title'>{ note.title }</p>
-            <p className='note-content'>{ note.content }</p>
-            <p className='note-date'>{ formattedDate }</p>
-            <button className='delete-button' onClick={() => onDelete(note.id)}>
-                Delete
-            </button>
-        </div>
-    );
+  const formattedDate = new Date(note.created_at).toLocaleDateString("en-US");
+
+  return (
+    <div className="note-container">
+      <p className="note-title">{note.title}</p>
+      <p className="note-content">{note.content}</p>
+      <p className="note-date">{formattedDate}</p>
+      <div className="note-actions">
+        <button className="delete-button" onClick={() => onDelete(note.id)}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Note
+export default Note;
