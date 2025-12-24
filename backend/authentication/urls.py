a326cfe39b6d2247e67554a8b4cd6e53acb83528
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2025-12-24 15:09:52 -0700

    fix: edit api endpoint according to conventions

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
-from . import views
+from django.urls import path, URLPattern, URLResolver
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
 
-
-urlpatterns = [
-    path("", views.UserCreate.as_view(), name="registration"),
-    path("token/get", TokenObtainPairView.as_view(), name="getToken"),
-    path("token/refresh", TokenRefreshView.as_view(), name="refreshToken"),
+urlpatterns: list[URLPattern | URLResolver] = [
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
