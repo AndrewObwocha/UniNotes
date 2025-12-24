fb7e869ea0ede3ab7813f5d446ae99cd41aca5cd
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2025-12-24 16:09:36 -0700

    chore: change db name

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'uninotes'),
-        'USER': os.environ.get('DB_USER', 'uninotes'),
+        'NAME': os.environ.get('DB_NAME', 'app'),
+        'USER': os.environ.get('DB_USER', 'app'),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '5432'),
